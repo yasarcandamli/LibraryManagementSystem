11ffaa57c27d2b9a5647facf5d7761214f03b9d7
yasarcandamli <yasarcandamli@gmail.com>
2024-07-06 12:39:00 +0300

    Author entity created

---
+++ src/main/java/dev/patika/Author.java
@@ -0,0 +1,66 @@
+package dev.patika;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "author_id", columnDefinition = "serial")
+    private int id;
+
+    @Column(name = "author_name")
+    private String name;
+
+    @Column(name = "author_birthDate")
+    private String birthDate;
+
+    @Column(name = "author_country")
+    private String country;
+
+    public Author() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
