82698261056f7b16223318e87f999f50c87a74bf
yasarcandamli <yasarcandamli@gmail.com>
2024-07-06 12:55:35 +0300

    BookBorrowing entity created

---
+++ src/main/java/dev/patika/BookBorrowing.java
@@ -0,0 +1,70 @@
+package dev.patika;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "book_borrowings")
+public class BookBorrowing {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_borrowing_id", columnDefinition = "serial")
+    private int id;
+
+    @Column(name = "borrower_name")
+    private String borrowerName;
+
+    @Column(name = "borrowing_date")
+    @Temporal(TemporalType.DATE)
+    private LocalDate borrowingDate;
+
+    @Column(name = "return_date")
+    @Temporal(TemporalType.DATE)
+    private LocalDate returnDate;
+
+    public BookBorrowing() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBorrowerName() {
+        return borrowerName;
+    }
+
+    public void setBorrowerName(String borrowerName) {
+        this.borrowerName = borrowerName;
+    }
+
+    public LocalDate getBorrowingDate() {
+        return borrowingDate;
+    }
+
+    public void setBorrowingDate(LocalDate borrowingDate) {
+        this.borrowingDate = borrowingDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    @Override
+    public String toString() {
+        return "BookBorrowing{" +
+                "id=" + id +
+                ", borrowerName='" + borrowerName + '\'' +
+                ", borrowingDate=" + borrowingDate +
+                ", returnDate=" + returnDate +
+                '}';
+    }
+}
