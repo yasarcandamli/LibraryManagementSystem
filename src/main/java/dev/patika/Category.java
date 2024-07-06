b27b3d0df78376a5136acb5ff2173086fa412775
yasarcandamli <yasarcandamli@gmail.com>
2024-07-06 12:42:51 +0300

    Category entity created

---
+++ src/main/java/dev/patika/Category.java
@@ -0,0 +1,54 @@
+package dev.patika;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", columnDefinition = "serial")
+    private int id;
+
+    @Column(name = "category_name")
+    private String name;
+
+    @Column(name = "category_description")
+    private String description;
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
